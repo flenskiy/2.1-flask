677eac658df8e75883bbf407acf993dd01acacc6
flenskiy <lenskiy.fv@gmail.com>
2023-03-16 18:34:02 +0300

    add docker-compose

--- db.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-
-from models import create_tables
-
-DSN = "postgresql://postgres:postgres@localhost:5431/app"
-engine = create_engine(DSN)
-Session = sessionmaker(engine)
-create_tables(engine)
